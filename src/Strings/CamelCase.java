a01afe886790dabeb4e7b2a0d6316c4d9bf99b23
Dinidu21 <dinidusachinth3@gmail.com>
2024-09-06 02:01:39 +0530

    CamelCase

---
+++ src/Strings/CamelCase.java
@@ -0,0 +1,49 @@
+package Strings;
+
+import java.io.*;
+
+
+class CamelCaseResult {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+        String [] ar = s.split("[A-Z]");
+        return ar.length;
+    }
+    /*
+         int count = 1;  // Starts with 1 since the first word is lowercase.
+        // Loop through the string to count uppercase letters
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isUpperCase(s.charAt(i))) {
+                count++;
+            }
+        }
+
+        return count;
+    */
+
+}
+
+public class CamelCase {
+    public static void main(String[] args) throws IOException {
+       BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = CamelCaseResult.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
